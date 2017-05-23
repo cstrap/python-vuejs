9a07b78eeb951469fe20dac038653965140cbb15
Christian Strappazzon <christian.strappazzon@indicod-ecr.it>
2017-05-23 17:47:00 +0200

    init djangofy vue.js

--- python_vuejs/django.py
+++ python_vuejs/django.py
@@ -1,11 +1,38 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
 
 import click
+import os
 
 
 @click.command()
-def djan(args=None):
-    """Console script for python_vuejs"""
-    click.echo("Replace this message by putting your code into "
-               "python_vuejs.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.argument('project')
+def djangobuild(project):
+    """
+    Called inside `package.json`
+    """
+    template_file = 'templates/{project}/index.html'.format(project=project)
+    with open(template_file, "r+") as f:
+        lines = f.readlines()
+        f.seek(0)
+        lines.insert(0, "{% load staticfiles %}\n")
+        for line in lines:
+            f.write(line.replace('href=/', "href={% static '/")
+                    .replace('.css', ".css' %}")
+                    .replace('src=/', "src={% static '/")
+                    .replace('.js', ".js' %}"))
+
+
+@click.command()
+@click.argument('project')
+def djangofy(project):
+    """
+    Convert Vue.js webpack project into a django app
+    """
+    os.makedirs('{project}/templates/{project}/'.format(project=project))
+    # TODO:
+    # create urls.py
+    # create __init__.py
+    # create empty index.html under templates/project
+    # edit `index.json` build
+    # edit `package.json` build
