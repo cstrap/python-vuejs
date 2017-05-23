a8f7f3a203cd11f5e08fee3a27f9a8cf48f989e9
cstrap <cstrap@users.noreply.github.com.com>
2017-05-23 17:11:59 +0200

    Vue.js wrapper via vue-cli provided by click

---
+++ python_vuejs/utils.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import contextlib
+import os
+
+
+@contextlib.contextmanager
+def cd(path):
+    """
+    A context manager which changes the working directory to the given
+    path, and then changes it back to its previous value on exit.
+    """
+    prev_cwd = os.getcwd()
+    os.chdir(path)
+    yield
+    os.chdir(prev_cwd)
